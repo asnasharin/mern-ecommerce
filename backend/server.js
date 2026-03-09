14c82b24a009fbacc0bec3e42154601c5e71e695
asna sharin <asnasherin1233@gmail.com>
2026-03-09 21:21:40 +0530

    add mongodb database connection

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
-const express = require("express")
+const express = require("express");
+const { connectDb } = require("./src/config/mongoConnect");
 
-const app = express()
 
-app.use(express.json())
+const app = express();
+
+require("dotenv").config()
+app.use(express.json());
+
+connectDb();
 
 app.get("/", (req, res) => {
     res.send("api is running")
-})
+});
 
-app.listen(5000, () => {
+app.listen(process.env.PORT, () => {
     console.log("server is running")
-})
+});
