424bf9eec32a20f646f277dcb67645fa47de335a
asna sharin <asnasherin1233@gmail.com>
2026-04-10 19:57:43 +0530

    fix

--- backend/src/routes/productRoute.js
+++ backend/src/routes/productRoute.js
@@ -1,5 +1,5 @@
 const { Router } = require ("express");
-const { createProductController, createProductReview, deleteProductController, deleteReview, getAllProduct, getAllProductsAdmin, getAllReviews, getProductDetails, updateProduct } = require("../controllers/productController.js");
+const { createProductController, createProductReview, deleteProductController, deleteReview, getAllProduct, getAllProductsAdmin, getAllReviews, getProductDetails, updateProduct } = require("../controllers/productController");
 const { isAdmin, protect } = require("../middlewares/authMiddleware.js");
 
 
